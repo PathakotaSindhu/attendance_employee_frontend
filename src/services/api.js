670138ee361d9b8380898be3052c27227e1e1487
PathakotaSindhu <pathakotasindhu01@gmail.com>
2025-04-29 23:06:30 +0530

    changed url inn api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-     baseURL: 'https://employee-attendance-31ex.onrender.com/api', // Your backend API base URL
+     baseURL: 'https://employee-attendance-31ex.onrender.com/api/auth',
     //baseURL: 'http://localhost:8080/api/auth',
     headers: {
        'Content-Type': 'application/json',
